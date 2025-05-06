1dbca739b0cc1df3021c643cb88582e5da81fb83
lucalizza26 <127794710+lucalizza26@users.noreply.github.com>
2025-05-06 15:03:52 +0200

    ber

--- BER and trimming/ber.py
+++ BER and trimming/ber.py
@@ -4,23 +4,36 @@ import csv
 def signal_to_bits(signal):
     # Convert signal to bit: 1 if non-zero, 0 if zero or absent
     try:
-        return 1 if float(signal) >= 1  else 0
+        return 1 if float(signal) >= 0.1  else 0  #SET THRESHOLD
     except ValueError:
         # Handle noise or invalid values by treating them as 0
+        print(f"Invalid signal value: {signal}. Treating as 0.")
         return 0
 
 # Read the CSV file and extract the bit sequence from the second column
-bit_sequences = []
-csv_file_path = 'pico_total_trimmed.csv'  # Replace with the actual path to your CSV file
+bits_sent = []
+bits_received = []
+csv_file_path = 'BER and trimming/pico_total_trimmed.csv'  # Replace with the actual path to your CSV file
 
 with open(csv_file_path, mode='r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader)  # Skip the header row if it exists
     for row in csv_reader:
         signal_sent = row[1]  # Assuming the second column is at index 1
-        bit_sequence = signal_to_bits(signal_sent)
-        bit_sequences.append(bit_sequence)
+        signal_received = row[2]
+        bit_sequence_sent = signal_to_bits(signal_sent)
+        bit_sequence_received = signal_to_bits(signal_received)
+        bits_sent.append(bit_sequence_sent)
+        bits_received.append(bit_sequence_received)
 
-print(bit_sequences[0:100])  # Print the first 10 bits for verification
-print(len(bit_sequences))
-print(bit_sequences.count(1)/len(bit_sequences)) # Percentage of 1s in the bit sequence
+count = 0
+
+for i in range(len(bits_sent)):
+    if bits_sent[i] != bits_received[i]:
+        # If the bits are different, increment the count
+        count += 1
+        print(f"Error at index {i}: Sent {bits_sent[i]}, Received {bits_received[i]}")
+
+
+print("Number of errors: ", count)  # Print the number of errors
+print("Bit Error Rate (BER): ", count/len(bits_sent)*100, " %")  # Print the Bit Error Rate (BER)
--- BER and trimming/picocsv.py
+++ BER and trimming/picocsv.py
@@ -18,7 +18,7 @@ for i in range(number_of_files):  # 0-tot
     csv_file_path = f'{folder_name}/pico_{i+1:03}.csv'  # Format i+1 as a zero-padded 3-digit number
     csv_data = read_csv(csv_file_path)
 
-    for row in csv_data[3::10000]:  # Start from the 4th row and take every 100th row
+    for row in csv_data[3::100]:  # Start from the 4th row and take every 100th row
         row = [value.replace(',', '.') if isinstance(value, str) else value for value in row]  # Replace commas with dots
         row[0] = str(i) + row[0]  # Add i to the first value in each row
         row[0] = str(float(row[0]) / 1000)  # Divide the first column by 1000
