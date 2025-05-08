3e50e0b9763e7ac8da23e0fc0273217b1ab6fea5
Jellevdm <j.h.c.vandermeer@gmail.com>
2025-05-08 11:58:53 +0200

    ensured plotting of the PDF and BER curves

--- pdf2ber.py
+++ pdf2ber.py
@@ -1,9 +1,13 @@
 import numpy as np
 from scipy.special import erfc
 import matplotlib.pyplot as plt
-from scipy.special import iv  # iv is the modified Bessel function of the first kind, # can be used to check against the simulation to determine correctness of the simulation.
-from scipy.special import besseli                        
-  
+from scipy.special import iv        # iv is the modified Bessel function of the first kind, # can be used to check against the simulation to determine correctness of the simulation.
+                      
+
+w0 = 0.9688e-3
+sigma_pj = 3.29e-5
+mu = 0.1e-3
+
 def pdfIGauss(w0, sigmaPJ, mu):
     """
     Computes the probability density function (PDF) of the irradiance fluctuations 
@@ -19,7 +23,7 @@ def pdfIGauss(w0, sigmaPJ, mu):
     hp  : The corresponding intensity fluctuation values (range from 0 to 1)
     """
     gamma = w0 / (2 * sigmaPJ)  # Compute gamma
-    hp = np.linspace(0, 1, 1001)  # Define hp values from 0 to 1
+    hp = np.linspace(0.1, 1, 1001)  # Define hp values from 0 to 1
 
     # Compute the argument for the modified Bessel function
     Z = (mu / sigmaPJ**2) * np.sqrt(-w0**2 * np.log(hp) / 2)
@@ -44,8 +48,16 @@ def pdf2ber(pdf, u):
     
     return BER, SNR
 
-pdf = pdfIGauss()
+pdf, u = pdfIGauss(w0, sigma_pj, mu)
 BER, SNR = pdf2ber(pdf, u)
+
+# Plot the PDF
+plt.plot(u, pdf)
+plt.xlabel("Normalized Irradiance (h')")
+plt.ylabel("PDF")
+plt.title(f'PDF of Irradiance Fluctuations - static pointing error: {mu} [m]')
+plt.grid(True)
+plt.show()
 
 # Plot the BER curve
 plt.semilogy(SNR, BER)  # Log scale for BER
