c743cbaeea3e6e002f34675558fc7b89e108d298
Jellevdm <93992711+Jellevdm@users.noreply.github.com>
2025-06-22 21:38:18 +0200

    .

--- Simulation files/simulation.py
+++ Simulation files/simulation.py
@@ -17,10 +17,13 @@ config_path = "config.toml"
 with open(config_path, 'rb') as f:          # 'rb' mode is required for tomllib
     config = tom.load(f)                    # Use tomllib to load the file
 
+optical_link_design = OpticalLinkBudget(config, "inputs_design", "losses_design")   # Initialise optical link budget class for our design                   
+link_budget_des = optical_link_design.compute_link_budget()
+
 # # Results of time simulation
 # #-----------------------------------------------
-# L_c = 10 ** (link_budget_des["Total losses const [db]"] / 10)  # Constant loss
-L_c = 1         #Due to incorrect link budget constant losses set to zero
+L_c = 10 ** (link_budget_des["Total losses const [db]"] / 10)  # Constant loss
+# L_c = 1         #Due to incorrect link budget constant losses set to zero
 signal_sim = Signal_simulation(config, file_name, "inputs_design", "inputs_signal", L_c)     # Initialse time signal simulation class    
 result_sim1 = signal_sim.generate_time_sig_square()
 
